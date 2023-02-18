7e6c49e41448ca4ae100533135c0e972e92769ad
Raamishshah010 <raamishshah010@gmail.com>
2023-02-18 13:22:18 +0500

    Firebase Config Done

---
+++ assets/api/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDDQYaon75oY-G7eHXAcD8cpnAtKwgOXGU",
+    authDomain: "firstfirebase-514ae.firebaseapp.com",
+    projectId: "firstfirebase-514ae",
+    storageBucket: "firstfirebase-514ae.appspot.com",
+    messagingSenderId: "348479979772",
+    appId: "1:348479979772:web:0d44fc8346d5d1c8075779"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+let db = firebase.firestore();
